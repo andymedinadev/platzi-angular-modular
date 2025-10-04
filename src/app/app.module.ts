937d8f786b72990999c14c06e01d04cb619de719
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-10-04 15:00:18 -0300

    Crear boilerplate de próximas páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,14 @@ import { ImgComponent } from './components/img/img.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MyCartComponent } from './pages/my-cart/my-cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
@@ -20,6 +28,14 @@ import { TokenInterceptor } from './interceptors/token.interceptor';
   declarations: [
     AppComponent,
     ImgComponent,
+    HomeComponent,
+    NotFoundComponent,
+    CategoryComponent,
+    MyCartComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    ProfileComponent,
     ProductComponent,
     ProductsComponent,
     NavComponent,
