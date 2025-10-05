dd3192fc59100a6a78b9215269b098360b01cd3a
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-10-05 17:48:59 -0300

    Separar en directorio Website para empezar a modularizar la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,44 +6,44 @@ import { SwiperModule } from 'swiper/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImgComponent } from './components/img/img.component';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductsComponent } from './components/products/products.component';
-import { NavComponent } from './components/nav/nav.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { CategoryComponent } from './pages/category/category.component';
-import { MyCartComponent } from './pages/my-cart/my-cart.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { RecoveryComponent } from './pages/recovery/recovery.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { TimeAgoPipe } from './pipes/time-ago.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
+import { CategoryComponent } from './website/pages/category/category.component';
+import { HighlightDirective } from './website/directives/highlight.directive';
+import { HomeComponent } from './website/pages/home/home.component';
+import { ImgComponent } from './website/components/img/img.component';
+import { LoginComponent } from './website/pages/login/login.component';
+import { MyCartComponent } from './website/pages/my-cart/my-cart.component';
+import { NavComponent } from './website/components/nav/nav.component';
+import { NotFoundComponent } from './website/pages/not-found/not-found.component';
+import { ProductComponent } from './website/components/product/product.component';
+import { ProductsComponent } from './website/components/products/products.component';
+import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
+import { ProfileComponent } from './website/pages/profile/profile.component';
+import { RecoveryComponent } from './website/pages/recovery/recovery.component';
+import { RegisterComponent } from './website/pages/register/register.component';
+import { ReversePipe } from './website/pipes/reverse.pipe';
+import { TimeAgoPipe } from './website/pipes/time-ago.pipe';
 import { TimeInterceptor } from './interceptors/time.interceptor';
 import { TokenInterceptor } from './interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgComponent,
-    HomeComponent,
-    NotFoundComponent,
     CategoryComponent,
-    MyCartComponent,
+    HighlightDirective,
+    HomeComponent,
+    ImgComponent,
     LoginComponent,
-    RegisterComponent,
-    RecoveryComponent,
-    ProductDetailComponent,
+    MyCartComponent,
+    NavComponent,
+    NotFoundComponent,
     ProductComponent,
     ProductsComponent,
+    ProductDetailComponent,
     ProfileComponent,
-    NavComponent,
+    RecoveryComponent,
+    RegisterComponent,
     ReversePipe,
     TimeAgoPipe,
-    HighlightDirective,
   ],
   imports: [
     BrowserModule,
